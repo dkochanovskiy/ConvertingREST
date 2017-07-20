f07a0f0fdb57a13e15e653c6239895128bfa9646
Денис <dkochanovskiy@gmail.com>
2017-07-20 12:12:30 +0300

    Results are displayed on the web-page

---
+++ src/main/java/ru/kochanovskiy/convertingrest/Controllers/ConverterController.java
@@ -0,0 +1,44 @@
+package ru.kochanovskiy.convertingrest.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.kochanovskiy.convertingrest.Services.ConverterService;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/")
+public class ConverterController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String converter() {
+        return "converter";
+    }
+
+    @RequestMapping(value= "/", method = RequestMethod.POST)
+    public String getNumber(ModelMap model, HttpServletRequest req) {
+
+        int enteredNumber = Integer.parseInt(req.getParameter("number"));
+
+        String unitsRu[] = {"ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять",
+                "десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"};
+
+        String unitsEn[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+                "ten", "eleven", "twelve", "thirteen ", "fourteen", "fifteen ", "sixteen", "seventeen", "eighteen", "nineteen"};
+
+        String tensRu[] = {"", "", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"};
+        String tensEn[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+        String hundredsRu[] = {"", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"};
+        String hundredsEn[] = {"", "one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"};
+
+        ConverterService converterService = new ConverterService();
+
+        model.addAttribute("decimalRu", converterService.decimalParser(Integer.toString(enteredNumber), unitsRu, tensRu, hundredsRu));
+        model.addAttribute("octalRu", converterService.decimalParser(converterService.octalConversion(enteredNumber), unitsRu, tensRu, hundredsRu));
+        model.addAttribute("decimalEn", converterService.decimalParser(Integer.toString(enteredNumber), unitsEn, tensEn, hundredsEn));
+        model.addAttribute("octalEn", converterService.decimalParser(converterService.octalConversion(enteredNumber), unitsEn, tensEn, hundredsEn));
+
+        return "converter";
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/converter.jsp
@@ -0,0 +1,35 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Converter</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+<br/>
+<form class="form-horizontal" action="/" method="post">
+    <div class="form-group">
+        <label for="number" class="col-sm-2 control-label">Введите число / Enter the number</label>
+        <div class="col-sm-2">
+            <input type="text" class="form-control" id="number" name="number" placeholder="">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-success">Конвертировать / Converting</button>
+        </div>
+    </div>
+</form>
+<p>${decimalRu}</p>
+<p>${octalRu}</p>
+<p>${decimalEn}</p>
+<p>${octalEn}</p>
+</body>
+</html>
